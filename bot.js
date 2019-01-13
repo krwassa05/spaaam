1a95233f5083d30ed24b194900512032e8d37c76
krwassa05 <46630037+krwassa05@users.noreply.github.com>
2019-01-13 13:24:41 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("533983604590510090")
+setInterval(function() {
+channel.send(`krwassa spam credit hala wtf ana hway`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
